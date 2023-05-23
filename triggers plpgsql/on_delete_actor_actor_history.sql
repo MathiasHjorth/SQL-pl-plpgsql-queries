c0f7fe18c9dcab83cc8f002d53d5e96bc76eb606
MathiasHjorth <mathiasbirgerhjorth95@gmail.com>
2023-05-23 11:07:30 +0200

    added triggers for dvd_rental_barebone

--- triggers plpgsql/on_delete_actor_actor_history.sql
+++ triggers plpgsql/on_delete_actor_actor_history.sql
@@ -1,7 +1,7 @@
---lav en trigger der ved insert af en række i actor table
+--lav en trigger der ved deletion af en række i actor table
 --laver en insert i actor_history, med tidspunkt og operationstypen, 
 --samt tilhørende attributer for actor
-CREATE OR REPLACE FUNCTION inserted_at()
+CREATE OR REPLACE FUNCTION deleted_at()
 RETURNS TRIGGER
 LANGUAGE plpgsql
 AS
@@ -10,13 +10,13 @@ $$
 		INSERT INTO actor_history(id,operation_type,time,first_name,last_name)
 		--NEW referer til den nye række skabt i actor
 		--OLD referer til rækken før operationen, hvis den eksisterer
-		VALUES (nextval('id_actor_history_seq')::integer,'Inserted',now(),NEW.first_name,NEW.last_name);
-	RETURN NEW;
+		VALUES (nextval('id_actor_history_seq')::integer,'Deleted',now(),OLD.first_name,OLD.last_name);
+	RETURN OLD;
 END;
 $$
 
-CREATE TRIGGER inserted_at
-AFTER INSERT
+CREATE TRIGGER deleted_actor_at
+BEFORE DELETE
 ON actor
 FOR EACH ROW
-EXECUTE PROCEDURE inserted_at()
+EXECUTE PROCEDURE deleted_at()
